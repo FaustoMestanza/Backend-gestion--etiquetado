3a48a68852646d92aecfbac8ba9b81c12f6bfcfb
FaustoMestanza <134259861+FaustoMestanza@users.noreply.github.com>
2025-10-13 16:06:00 -0500

    Fix staticfiles para Whitenoise en Azure

--- gestion_qr/settings.py
+++ gestion_qr/settings.py
@@ -96,7 +96,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+SSTATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
